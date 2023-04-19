a4a501e650cab1534d96d490dd1e2530345dafa7
Tyler Yu <tyler.y.yu@gmail.com>
2023-04-18 20:19:10 -0700

    animate on scroll for quotes
    top bar stays on top of animated quotes
    
    bottom up animation of quote boxes
    
    Revert "mui import"
    
    This reverts commit 1a5ac4460e8466f4cedb06c4f0852b6143bd4b08.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,35 +48,37 @@ function App() {
 	return (
 		<div className="App">
 			
-			<div className="center-topbar">
+			<div className="center-menu">
+				<div className="center-topbar">
 
-				<div className="topbar">
-					<div className="topbar-text">
+					<div className="topbar">
+						<div className="topbar-text">
 
-						<div className="title">
-							<h1 className="deliverable-header">Hack @ UCI Tech Deliverable</h1>
-							<h2 className="submit-quote-text">Submit a quote</h2>
+							<div className="title">
+								<h1 className="deliverable-header">Hack @ UCI Tech Deliverable</h1>
+								<h2 className="submit-quote-text">Submit a quote</h2>
+							</div>
+							<img 
+								className="quote-image"
+								src="https://file.notion.so/f/s/c9f6ea9f-45de-4088-a0c9-2563f8e4b4b4/quotebook.png?id=523e9bd3-b49e-4db4-af12-132698ae81dd&table=block&spaceId=4e23f586-3e2e-403c-b0fd-e532279b0d08&expirationTimestamp=1681623817510&signature=kS5ssaNgmRI3fVcdbOIYevkBag2IdvqfqkERcxMNeVE&downloadName=quotebook.png"
+							/>
+
 						</div>
-						<img 
-							className="quote-image"
-							src="https://file.notion.so/f/s/c9f6ea9f-45de-4088-a0c9-2563f8e4b4b4/quotebook.png?id=523e9bd3-b49e-4db4-af12-132698ae81dd&table=block&spaceId=4e23f586-3e2e-403c-b0fd-e532279b0d08&expirationTimestamp=1681623817510&signature=kS5ssaNgmRI3fVcdbOIYevkBag2IdvqfqkERcxMNeVE&downloadName=quotebook.png"
-						/>
+						
+						<div className="submit-quote-section">
+							<form id="quote-form" className="submit-elems">
+								<label htmlFor="input-name">Name</label>
+								<input type="text" name="name" id="input-name" value={name} onChange={(e) => changeName(e.target.value)} required />
+								<label htmlFor="input-message">Quote</label>
+								<input type="text" name="message" id="input-message" value={quote} onChange={(e) => changeQuote(e.target.value)} required />
+								<button type="submit" onClick={handleSubmit}>Submit</button>
+							</form>
+						</div>
+						
+					</div>
 
-					</div>
-					
-					<div className="submit-quote-section">
-						<form id="quote-form" className="submit-elems">
-							<label htmlFor="input-name">Name</label>
-							<input type="text" name="name" id="input-name" value={name} onChange={(e) => changeName(e.target.value)} required />
-							<label htmlFor="input-message">Quote</label>
-							<input type="text" name="message" id="input-message" value={quote} onChange={(e) => changeQuote(e.target.value)} required />
-							<button type="submit" onClick={handleSubmit}>Submit</button>
-						</form>
-					</div>
-					
+
 				</div>
-
-
 			</div>
 
 			<h2 className="prevQuoteText">Previous Quotes</h2>
--- frontend/src/components/QuoteBox.jsx
+++ frontend/src/components/QuoteBox.jsx
@@ -1,10 +1,26 @@
 import "./QuoteBox.css"
+import { useEffect, useRef, useState } from "react";
 
 const QuoteBox = ({author, quote, date}) => {
+    const [intersectionRatio, changeIntersecting] = useState(0)
+    const ref = useRef();
+
+    useEffect(  () => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach( (entry) => {
+                changeIntersecting(entry.intersectionRatio)
+            })
+        })
+
+        observer.observe(ref.current)
+        return () => {
+            observer.unobserve(ref.current)
+        }
+    }, []);
 
 
     return (
-        <div className="QuoteBox">
+        <div ref={ref} className={`QuoteBox fade-in ${intersectionRatio > 0 ? "is-visible" : ""}`}>
             <p className="author-text">{author}</p>
             <p className="quote-text">{quote}</p>
             <p className="date-text">{date}</p>
